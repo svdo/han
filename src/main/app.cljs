137695ff9235399a3c2f501d48969f73ce16c34c
Stefan van den Oord <soord@mac.com>
2019-04-24 10:03:50 +0200

    Converts JavaScript to ClojureScript

---
+++ src/main/app.cljs
@@ -0,0 +1,47 @@
+(ns app
+  (:require [reagent.core :as r :refer [atom]]
+            ["react-native" :as rn :refer [AppRegistry, Platform]]))
+
+(def styles
+  {:container
+   {:flex 1
+    :justify-content "center"
+    :align-items "center"
+    :background-color "#F5FCFF"}
+
+   :welcome
+   {:font-size 20
+    :text-align "center"
+    :margin 10}
+
+   :instructions
+   {:text-align "center"
+    :color "#333333"
+    :margin-bottom 5}})
+
+(def instructions
+  (.select Platform
+           #js {:ios "Press Cmd+R to reload,\nCmd+D or shake for dev menu"
+                :android "Double tap R on your keyboard to reload,\nShake or press menu button for dev menu"}))
+
+(defn app-root []
+  [:> rn/View {:style (:container styles)}
+   [:> rn/Text {:style (:welcome styles)} "Welcome to React Native!"]
+   [:> rn/Text {:style (:instructions styles)} "To get started, edit app.cljs"]
+   [:> rn/Text {:style (:instructions styles)} instructions]])
+
+(defonce app-root-ref (atom nil))
+
+(def updatable-app-root
+  (with-meta app-root
+    {:component-did-mount
+     (fn [this]
+       (println "component did mount")
+       (reset! app-root-ref this))}))
+
+(defn init []
+  (println "Hello!")
+  (.registerComponent AppRegistry "Han" #(r/reactify-component updatable-app-root)))
+
+(defn ^:dev/after-load reload []
+  (.forceUpdate @app-root-ref))
