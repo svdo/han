99f4c25d16fc3d12ad3b3757ef4ddb33ac808dad
Stefan van den Oord <soord@mac.com>
2019-05-11 22:00:37 +0200

    Uses `injectJavaScript` instead of `injectedJavaScript`
    Works around issue https://github.com/react-native-community/react-native-webview/issues/554.
    Note that with this method the REPL inside the webview works for iOS; on Android
    it immediately says "shadow-cljs - Connection closed!".

--- src/main/player.cljs
+++ src/main/player.cljs
@@ -2,9 +2,36 @@
   (:require ["react-native-webview" :as rnwv :refer [WebView]]
             [shadow.resource :as rc]))
 
+(defonce javascript-injected (atom false))
+
+(defn inject-javascript [ref]
+  (when (and (not (nil? ref))
+             (false? @javascript-injected))
+    (reset! javascript-injected true)
+    (def replace-console-log
+      (str "var console={};"
+           "console.log=function(message){"
+           "  document.body.innerHTML+='<p>'+JSON.stringify(arguments)+'</p>';"
+           "  if (arguments.length > 2 && arguments[2].message){"
+           "    document.body.innerHTML+='<p>'+arguments[2].message+'<br/><pre>'+arguments[2].stack+'</pre></p>'"
+           "  };"
+           "};"
+           "console.error=console.log;"
+           "console.exception=console.error;"
+           "console.warn=console.log;"
+           "window.console=console;"))
+    (def audio-engine-source (str replace-console-log
+                                  (rc/inline "audio-engine-compiled-js-source.txt")))
+    (def javascript (str replace-console-log audio-engine-source ";true"))
+
+    (js/setTimeout
+     #(.injectJavaScript ref javascript) 1000)))
+
 (defn Player []
-  (def audio-engine-source (str (rc/inline "audio-engine-compiled-js-source.txt") ";true"))
-  [:> WebView {:media-playback-requires-user-action false
+  [:> WebView {:ref (fn [r] (inject-javascript r))
+               :media-playback-requires-user-action false
+               :java-script-enabled true
+               :allow-file-access true
+               :allow-universal-access-from-file-URLs true
                :origin-whitelist ["*"]
-               :source {:html "<body style='font-size: 200%'><h1>Hi!</h1></body>"}
-               :injected-java-script audio-engine-source}])
+               :source {:html "<body style='font-size: 200%'><h1>Hi!</h1></body>"}}])
