b0a88e277c775a97e45c5b83ab0bbec9cb7d6537
Stefan van den Oord <soord@mac.com>
2019-05-11 22:00:56 +0200

    Adds loads of debug logging

--- src/main/audio/engine.cljs
+++ src/main/audio/engine.cljs
@@ -19,10 +19,11 @@
   (js/console.log "audioContext (console.log)" @audioContext))
 
 (defn play []
+  (add-to-body "<p>play?</p>")
   (when (not @unlocked)
     (let [buffer (.createBuffer @audioContext 1 1 22050)
-          node [(.createBufferSource @audioContext)]]
-      (add-to-body "<p>play</p>")
+          node (.createBufferSource @audioContext)]
+      (add-to-body (str "<p>play: " node "</p>"))
       (goog.object/set node "buffer" buffer)
       (.start node 0)
       (reset! unlocked true))))
@@ -32,18 +33,33 @@
         sustain 0.03
         time (.-currentTime @audioContext)
         note-length 0.5
-        gain (.createGain @audioContext)
-        osc (.createOscillator @audioContext)]
-    (add-to-body "<p>schedule-note</p>")
+        gain (.createGain @audioContext)]
+    (add-to-body (str "<p>schedule-note at time " time "</p>"))
     (.setValueAtTime (.-gain gain) 0 time)
+    (add-to-body "1.")
     (.linearRampToValueAtTime (.-gain gain) 1 (+ time attack))
+    (add-to-body "2.")
     (.setValueAtTime (.-gain gain) 1 (+ time attack sustain))
+    (add-to-body "3.")
     (.linearRampToValueAtTime (.-gain gain) 0 (+ time note-length))
 
-    (.connect (.connect osc gain) (.-destination @audioContext))
-    (goog.object/set (.-value (.-frequency osc)) 880)
-    (.start osc time)
-    (.stop osc (+ time note-length))))
+    (add-to-body "4.")
+    (try
+      (let [osc (.createOscillator @audioContext)]
+        ; (.connect (.connect osc gain) (.-destination @audioContext))
+        ; (.connect osc (.-destination @audioContext))
+        (-> osc
+            ; (.connect gain)
+            (.connect (.-destination @audioContext)))
+        (goog.object/set (.-frequency osc) "value" 880)
+        (add-to-body "5.")
+        (add-to-body "6.")
+        (.start osc time)
+        (add-to-body "7.")
+        (.stop osc (+ time note-length))
+        (add-to-body "8."))
+      (catch js/Error e
+        (add-to-body e)))))
 
 (init)
 (schedule-note)
