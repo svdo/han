3b88577c3f617810c1cbf8f3717b97200e28544c
Stefan van den Oord <soord@mac.com>
2019-05-29 13:25:36 +0200

    Adds measure separators

--- src/main/staff.cljs
+++ src/main/staff.cljs
@@ -3,6 +3,7 @@
             [reagent.core :as r]))
 
 (defonce staff-img (js/require "../img/staff.png"))
+(defonce measure-separator (js/require "../img/separator.png"))
 
 (defn cell [details]
   (let [item (details "item")
@@ -10,6 +11,12 @@
     [:> rn/View {:style {:width 80
                          :background-color (if (= 0 (mod key 2)) "#dd666611" "#ffffff33")}}
      [:> rn/Text {:style {:left -15 :width 30 :text-align "center"}} key]]))
+
+(defn separator []
+  [:> rn/View {:style {:width 1}}
+   [:> rn/ImageBackground {:source measure-separator
+                           :resize-mode "center"
+                           :style {:width 1 :height 66}}]])
 
 (defn Staff [styles]
   [:> rn/View styles
@@ -19,4 +26,5 @@
     [:> rn/FlatList
      {:data (clj->js (mapv (fn [n] {:key n}) (range 0 100)))
       :horizontal true
+      :Item-separator-component #(r/as-element [separator])
       :render-item (fn [details] (r/as-element [cell (js->clj details)]))}]]])
