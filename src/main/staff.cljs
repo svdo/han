24804ad26d6c71c0a841ad414e82e50ae2a604ef
Stefan van den Oord <soord@mac.com>
2019-05-30 08:17:27 +0200

    Adds staff header

--- src/main/staff.cljs
+++ src/main/staff.cljs
@@ -5,6 +5,7 @@
 
 (defonce staff-img (js/require "../img/staff.png"))
 (defonce measure-separator (js/require "../img/separator.png"))
+(defonce staff-header (js/require "../img/staff-header.png"))
 
 (defn cell [details]
   (let [item (details "item")
@@ -13,6 +14,12 @@
     [:> ViewOverflow {:style {:width 80
                               :background-color (if (= 0 (mod num 2)) "#dd666611" "#ffffff33")}}
      [:> rn/Text {:style {:left -15 :width 30 :text-align "center"}} num]]))
+
+(defn header []
+  [:> rn/View {:style {:width 15}}
+   [:> rn/ImageBackground {:source staff-header
+                           :resize-mode "center"
+                           :style {:width 15 :height 66}}]])
 
 (defn separator []
   [:> rn/View {:style {:width 1}}
@@ -26,8 +33,9 @@
                            :resize-mode "stretch"
                            :style {:width "100%" :height 66}}
     [:> rn/FlatList
-     {:data (clj->js (mapv (fn [n] {:key (str n) :measure-number n}) (range 1 100)))
+     {:data (clj->js (mapv (fn [n] {:key (str n) :measure-number n}) (range 1 11)))
       :horizontal true
       :Cell-renderer-component ViewOverflow
+      :List-header-component #(r/as-element [header])
       :Item-separator-component #(r/as-element [separator])
       :render-item (fn [details] (r/as-element [cell (js->clj details)]))}]]])
