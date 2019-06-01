76fd868b3cec0f3714c001c123ac1137e063b964
Stefan van den Oord <soord@mac.com>
2019-06-01 13:14:08 +0200

    Puts image width in more logical place

--- src/main/staff.cljs
+++ src/main/staff.cljs
@@ -4,10 +4,10 @@
             [reagent.core :as r]
             [styles :refer [styles]]))
 
-(defonce staff-img (js/require "../img/staff.png"))
-(defonce measure-separator (js/require "../img/separator.png"))
-(defonce staff-header (js/require "../img/staff-header.png"))
-(defonce staff-footer (js/require "../img/staff-end.png"))
+(defonce staff-img         {:img (js/require "../img/staff.png")        :width 10})
+(defonce measure-separator {:img (js/require "../img/separator.png")    :width  1})
+(defonce staff-header      {:img (js/require "../img/staff-header.png") :width 15})
+(defonce staff-footer      {:img (js/require "../img/staff-end.png")    :width 11})
 
 (defn show-measure-num? [num]
   (or (= 1 num)
@@ -17,7 +17,7 @@
   (let [item (details "item")
         num (item "measure-number")]
     [:> ViewOverflow {:style {:width 68}}
-     [:> rn/ImageBackground {:source staff-img
+     [:> rn/ImageBackground {:source (:img staff-img)
                              :resize-mode "stretch"
                              :style {:width "100%" :height 66}}
       (if (show-measure-num? num) [:> rn/Text {:style (:measure-number styles)} num])
@@ -25,15 +25,16 @@
        [:> rn/Text {:style (:time-signature styles)} "3"]
        [:> rn/Text {:style (:time-signature styles)} "4"]]]]))
 
-(defn staff-component [width bg-image]
-  [:> rn/View {:style {:width width}}
-   [:> rn/ImageBackground {:source bg-image
-                           :resize-mode "center"
-                           :style {:width width :height 66}}]])
+(defn staff-component [image]
+  (let [width (:width image)]
+    [:> rn/View {:style {:width width}}
+     [:> rn/ImageBackground {:source (:img image)
+                             :resize-mode "center"
+                             :style {:width width :height 66}}]]))
 
-(defn header    [] (staff-component 15 staff-header))
-(defn separator [] (staff-component 1 measure-separator))
-(defn footer    [] (staff-component 11 staff-footer))
+(defn header    [] (staff-component staff-header))
+(defn separator [] (staff-component measure-separator))
+(defn footer    [] (staff-component staff-footer))
 
 (defn Staff [styles]
   [:> rn/View styles
