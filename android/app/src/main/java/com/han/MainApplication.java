67bcc3fc71de26fb88972be0fe38f7b1137e59fa
Stefan van den Oord <soord@mac.com>
2019-06-17 06:45:48 +0200

    Adds react-native-svg

--- android/app/src/main/java/com/han/MainApplication.java
+++ android/app/src/main/java/com/han/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.webkit.WebView;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.entria.views.RNViewOverflowPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new RNViewOverflowPackage(),
             new RNCWebViewPackage()
       );
